9f3e88779ba3b614f4a8711482da8de0e680f762
Ankur-Datta-4 <ankurr2002@gmail.com>
2023-07-20 23:30:27 +0530

    Co-authored-by: mrunalinithamankar <mrunalinithamankar@users.noreply.github.com> Co-authored-by: Nireeksha D Rai <Nireekshadrai@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask,render_template, request, jsonify
+from flask import Flask,render_template, request, jsonify, send_file, abort
 from generate_set import generate_sets
 from generate_polygon import generate_polygon
+import os
+import zipfile
+from time import sleep
+
+# Get the parent directory of this script. (Global)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 
@@ -16,10 +22,45 @@ def get_json_data():
     irregularity_clip = request.get_json()['irregularity_clip']
     spikiness_clip = request.get_json()['spikiness_clip']
     # Visualize the generated polygons
-    dataset_id = generate_sets(card, xsize, ysize, vertices_bound, show_grid=False,
+    dataset_id, for_visualizer = generate_sets(card, xsize, ysize, vertices_bound, show_grid=False,
                           irregularity_clip=irregularity_clip, spikiness_clip=spikiness_clip)
 
     # return 200 with dataset_id as json
-    return jsonify({'dataset_id': dataset_id})
+    return jsonify({'dataset_id': dataset_id, 'for_visualizer': for_visualizer})
 
-app.run(debug=True)
+
+# go to outputs/:id and then send the files
+# for download
+
+OUTPUTS_DIRECTORY = 'outputs'
+
+def find_folder_by_uuid(uuid):
+    folder_path = os.path.join(OUTPUTS_DIRECTORY, uuid)
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        return folder_path
+    return None
+
+@app.route('/get_file/<string:uuid>', methods=['GET'])
+def get_folder_by_uuid(uuid):
+    folder_path = find_folder_by_uuid(uuid)
+    if not folder_path:
+        abort(404, f"Folder with UUID '{uuid}' not found.")
+
+    zip_file_path = folder_path + '.zip'
+    with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname=arcname)
+
+    # Send the zip archive to the client and delete it afterwards.
+    response=send_file(zip_file_path, as_attachment=True)
+    # os.remove(zip_file_path)
+    return response
+    # try:
+    #     return send_file(zip_file_path, as_attachment=True)
+    # finally:
+    #     os.remove(zip_file_path)
+
+app.run(debug=True)                                                             
--- generate_set.py
+++ generate_set.py
@@ -6,29 +6,18 @@ from download_csv import convert_to_shape_csv
 from shapely.geometry import Polygon
 from utils import clip
 from generate_polygon import generate_polygon
-
-
+import json
+import geopandas
 '''
   Time : O(N) , where N is number of vertices
   space : O(M)
 '''
-
-
 def generate_sets(card, xsize, ysize, vertices_bounds, show_grid=True, irregularity_clip=0.8, spikiness_clip=0.8):
-    # card=100
     gridCols = math.ceil(math.sqrt(card))
-    # 5*5=> 25 cells. Need to distribute card into these cells
     gridRows = math.ceil(math.sqrt(card))
-    # vertices_bounds=[3,10]
-    # xsize=1000
-    # ysize=1000
     print(card, gridCols, gridRows, gridCols * gridRows)
-    # print("sfwenfwefnweuifhnewj")
     if card > (gridCols*gridRows):
-        print("heeeeeerrrrre")
         card = gridCols*gridRows
-        # raise Exception("Cannot generate non-overlapping polygons")
-
     mapping = dict()  # key: rowNum, value:[colNum]
     generated_polygon_centers = 0
     while generated_polygon_centers != card:
@@ -52,51 +41,13 @@ def generate_sets(card, xsize, ysize, vertices_bounds, show_grid=True, irregular
       - vetices bound input
     2. Generate a polygon in these cells
   """
-
-    black = (0, 0, 0)
-    white = (255, 255, 255)
-    img = Image.new('RGB', (xsize, ysize), white)
-    im_px_access = img.load()
-    draw = ImageDraw.Draw(img)
-
     shapes = []
-    # center_bounds=[50, 400]
-    avg_radius = 50
-
     point = 0
-    if(show_grid):
-        height = ysize
-        width = xsize
-        image = Image.new(mode='L', size=(height, width), color=255)
-
-        # Draw some line
-        y_start = 0
-        y_end = image.height
-        step_size = int(xsize/gridRows)
-
-        for x in range(0, image.width, step_size):
-            line = ((x, y_start), (x, y_end))
-            draw.line(line, fill=128)
-
-        x_start = 0
-        x_end = image.width
-
-        for y in range(0, image.height, step_size):
-            line = ((x_start, y), (x_end, y))
-            draw.line(line, fill=128)
-
-    # if(20 > int(xsize/(2*gridCols))):
-    #   raise Exception("Cannot have a decreasing a randInt")
 
     for key in mapping.keys():
         for value in mapping[key]:
             centerx = (clip((xsize/(2*gridCols))+value*(xsize/gridCols), 0, xsize),
                        clip((ysize/(2*gridRows))+(key)*(ysize/(gridRows)), 0, ysize))
-            #  centerx=((value+1)*(xsize/gridCols), (key+1)*(ysize/gridRows))
-            #  print(centerx)
-            #  print(avg_radius)
-
-            # We can do parallel execution in this loop
 
             shapes.append(generate_polygon(center=centerx,
                                            avg_radius=random.randint(
@@ -106,22 +57,15 @@ def generate_sets(card, xsize, ysize, vertices_bounds, show_grid=True, irregular
                                            spikiness=clip(
                                                random.random(), 0, spikiness_clip),
                                            num_vertices=random.randint(vertices_bounds[0], vertices_bounds[1])))
-            # print(shapes[point])
-            draw.polygon(shapes[point], outline=black, fill=white)
-            draw.line(shapes[point] + [shapes[point][0]], width=2, fill=black)
-
-            #  break
             point += 1
 
-    # return image
     pols = []
     for i in shapes:
-        # i.append(i[0])
         p1 = Polygon(i)
         pols.append(p1)
-    
-    return convert_to_shape_csv(pols)
-    
-    # return img
 
-    # img.show()
+    #send 15 polygons for visualization which is json serialized
+    for_vis =pols[:15]
+  
+    return (convert_to_shape_csv(pols), geopandas.GeoDataFrame(geometry=for_vis).to_json())
+    
