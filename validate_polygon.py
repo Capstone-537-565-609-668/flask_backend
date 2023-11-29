f815fd10f8c4be9474dc9cbeebc851306a8f6692
Nireekshadrai <nireekshadrai@gmail.com>
2023-11-29 15:13:04 +0530

    [ADD] overllap elimination

--- validate_polygon.py
+++ validate_polygon.py
@@ -1,10 +1,10 @@
+import csv
+
 import geopandas as gpd
-from shapely.validation import explain_validity, make_valid
-from shapely.geometry import Polygon
 import matplotlib.pyplot as plt
 import pandas as pd
-import csv
-import geopandas as gpd
+from shapely.geometry import Polygon
+from shapely.validation import explain_validity, make_valid
 from shapely.wkt import loads
 
 
@@ -15,22 +15,44 @@ def correct_invalid_geometry(geometry):
     else:
         return geometry
 
+def number_of_invalid(shp):
+    count = 0
+    for i in shp.index:
+        if shp['validity'][i] != 'Valid Geometry':
+            count+=1
+    return count
 
 def validate_polygon(pols):
+    # Set a buffer distance to handle overlaps
+    buffer_distance = 0.00000001  # Adjust this value based on your data and precision needs
     geoseries = gpd.GeoSeries(pols)
     shp = gpd.GeoDataFrame(geoseries, columns=['geometry'])
-    overlaps = gpd.overlay(shp, shp, how='intersection')
+    
+    
 
-    # Merge the overlapping geometries
-    merged = gpd.overlay(shp, overlaps, how='union')
-    # Drop duplicate columns and reset index
-    merged = merged[shp.columns].reset_index(drop=True)
-    shp['geometry'] = merged['geometry'].apply(correct_invalid_geometry)
+
+
+    for i, row1 in shp.iterrows():
+        for j, row2 in shp.iterrows():
+            if i < j:
+                geometry1 = row1['geometry']
+                geometry2 = row2['geometry']
+
+                # Check for overlaps within the buffer distance
+                if geometry1.buffer(buffer_distance).intersects(geometry2):
+                    # Remove overlapping part from geometry2
+                    diff_geometry = geometry2.difference(geometry1)
+
+                    # Update the DataFrame with the modified geometry
+                    shp.at[j, 'geometry'] = diff_geometry
+    shp['geometry'] = shp['geometry'].apply(correct_invalid_geometry)
+
     multi_polygons = shp[shp['geometry'].geom_type == 'MultiPolygon']
     shp = shp[shp['geometry'].geom_type == 'Polygon']
-    if not multi_polygons.empty:
-        multi_polygons = multi_polygons.explode(index_parts=True)
-        shp = pd.concat([shp, multi_polygons], ignore_index=True)
-        return validate_polygon(shp)
-    
+    multi_polygons = multi_polygons.explode(index_parts=True)
+
+    # Concatenate the exploded MultiPolygons back to the original GeoDataFrame
+    shp = pd.concat([shp, multi_polygons], ignore_index=True)
+    # Iterate over the DataFrame to perform operations
+
     return shp['geometry'].tolist()
